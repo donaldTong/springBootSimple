13341a63025a0a83ebacc5f348fc84743b7017b1
donaldTong <donald>
2018-11-27 09:15:07 +0800

    springBoot项目快速生成

---
+++ src/main/java/com/tlj_example/springBoot/control/IndexControl.java
@@ -0,0 +1,16 @@
+package com.tlj_example.springBoot.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class IndexControl {
+
+	@RequestMapping(path = {"/index"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String index(){
+        return "index";
+    }
+}
